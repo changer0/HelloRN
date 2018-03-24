3ef51c51d1c0c70e8ba0d8001151445125370ad1
DESKTOP-H58BO6B\lulu <zhanglulu1210@gmail.com>
2018-03-24 10:34:52 +0800

    首次提交

---
+++ Test.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+export default class Test extends Component {
+  render() {
+    return (
+      <Text>
+        这是一个测试页面
+      </Text>
+    );
+  }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Test from './Test';
+
+AppRegistry.registerComponent('HelloRN', ()=>Test);
