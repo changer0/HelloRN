9fd9d66390237bf44cdc1ef681fe8e1c3fa45086
DESKTOP-H58BO6B\lulu <zhanglulu1210@gmail.com>
2018-03-25 17:06:09 +0800

    学习制作一个Button

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import FlexBox from './src/example/FlexBox';
-import TextInputExample from './src/example/TextInputExample';
-import IScrolledDownAndWhatHappenedNextShockedMe from './src/example/ScrollViewExample';
-import ButtonExample from './src/example/ButtonExample';
+import FlexBox from './src/pages/FlexBox';
+import TextInputExample from './src/pages/TextInputExample';
+import IScrolledDownAndWhatHappenedNextShockedMe from './src/pages/ScrollViewExample';
+import ButtonExample from './src/pages/ButtonExample';
 
 AppRegistry.registerComponent('HelloRN', ()=>ButtonExample);
